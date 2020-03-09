7b31558fdd58b93b63882cafd3c51f6e50128ce8
nobleoxford <nobleoxford@amdg.rockhursths.edu>
2020-03-09 08:50:12 -0500

    adv

---
+++ bess.py
@@ -0,0 +1,12 @@
+int main
+class game(object):
+    def __init__ (self):
+    self.run()
+    def game (self):
+    	menuinput=input("1 = start, 2 = exit")
+    	if menuinput=1:
+    		for i in range (25):
+    			diceroll=random(1,6)
+    			print("Dice roll: " + diceroll)
+    			playerPosition += diceroll
+    			if playerPosition=="6"
