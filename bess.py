5d5dbdc64ed7a714be58a4ffe01faf273f06cce8
nobleoxford <nobleoxford@amdg.rockhursths.edu>
2020-03-11 08:46:16 -0500

    Final Project Answer

--- bess.py
+++ bess.py
@@ -1,12 +1,17 @@
 import random
-plyerPosition = 0
+
+playerPosition = 0
 menuinput=input("1 = start, 2 = exit: ")
+print("Win when you Roll 2 6's")
 if menuinput == '1':
-    while playerPosition <= 25:
-        diceroll = randint(1,6)
-    	print("Dice roll: " + diceroll)
-    	playerPosition += diceroll
-    	    if playerPosition== '6':
-                    playerPosition -= 4
+    win = False
+    while win == False:
+        playerRoll = random.randint(1,6)
+        print("Dice roll: " + str(playerRoll))
+        if playerRoll == 6:
+            diceroll = random.randint(1,6)
+            if diceroll == 6:
+                win = True
+                print("You won the game")
                 
                     
